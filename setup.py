537fe272ead7999d5b2ea9d38f5052fc6bcba6c1
Jingyi Cao <jcao13@bwh.harvard.edu>
2024-06-14 11:07:20 -0400

    updated

--- setup.py
+++ setup.py
@@ -8,6 +8,12 @@ setup(
     version='1.0',
     packages=['scotia'],
     package_dir={'scotia': 'scotia'},
+    license="MIT",
+    description='scotia',
+    url='https://github.com/Caochris/SCOTIA',
+    python_requires='>=3.6',
+    author='Jingyi Cao',
+    author_email='jcao13@bwh.harvard.edu',
     install_requires=[
         'bezier',
         'forest-fire-clustering',
