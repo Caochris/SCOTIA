45aed2a081199ae668dda3225490291cc428d345
Jingyi Cao <jcao13@bwh.harvard.edu>
2025-07-07 19:15:46 -0400

    add version info

--- scotia/__init__.py
+++ scotia/__init__.py
@@ -3,3 +3,4 @@ from .ot import sel_pot_inter_cluster_pairs, source_target_ot, post_ot
 from .permutation import permutation_test
 from .visualization import curved_edges
 from .run_scotia import lr_score, run_dbscan, run_ot_analysis
+__version__ = "1.0.0"
